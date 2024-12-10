6c57133c030aaa893bf2d461d113f61eb2291ac2
Rayhan Abdul Rahman <rabdulr4@uwo.ca>
2024-12-10 00:05:44 -0500

    Update AbdulrahmanRayhanmohammed_Project4.py
    Edited function information, changed rounding of time of interest for plotting function and tested a few plots.

--- AbdulrahmanRayhanmohammed_Project4.py
+++ AbdulrahmanRayhanmohammed_Project4.py
@@ -51,12 +51,16 @@ def prob_integral(array):
 
 def sch_eqn(nspace, ntime, tau, method = 'ftcs', length = 200, potential = [], wparam = [10,0,0.5]):
     '''Function to solve 1 dimensional, time dependent Schroedinger Equation. Please provide the following:
-    number of spatial grid points, number of time steps to be evolved, and the time step to be used. Optional arguments are:
-    1. method: string, either ftcs or crank (Crank-Nicolson), 2. length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100),
-    3. potential: 1-D array giving the spatial index values at which the potential V(x) should be set to 1. Default to empty. ,
-    4. wparam: list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
+    1. nspace: int, number of spatial grid points, 
+    2. ntime: int, number of time steps to be evolved, 
+    3. tau: float, the time step to be used. 
+       The rest are optional and are:
+    4. method: string, either ftcs or crank (Crank-Nicolson), 
+    5. length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100),
+    6. potential: 1-D array giving the spatial index values at which the potential V(x) should be set to 1. Default to empty. ,
+    7. wparam: list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
     
-    Unit assumptions h_bar = 1 and m = 1/2. 
+    Solver assumptions: h_bar = 1 and m = 1/2. 
 
     Further information can be found in the code documentation; AbdulRahman_RayhanMohammed_project4.pdf'''
 
@@ -66,13 +70,13 @@ def sch_eqn(nspace, ntime, tau, method = 'ftcs', length = 200, potential = [], w
     t_vals = np.linspace(0, ntime * tau, ntime)
     psi = np.zeros((nspace,ntime), dtype=np.complex128)
 
-    j = 1j # defining root of -1
+    j = 1j # defining root of -1 to reduce complexity of later lines of code
 
     V = np.zeros(nspace) # Setting Potential
     for index in potential:
         V[index] = 1
 
-    probabilities = np.zeros(ntime) # Setting probabiliy to zero
+    probabilities = np.zeros(ntime) # Setting initial probabiliy to zero
 
     # Using initial conditions from wparam
     sigma = wparam[0]
@@ -83,8 +87,12 @@ def sch_eqn(nspace, ntime, tau, method = 'ftcs', length = 200, potential = [], w
     #initial row using 9.42 from textbook
     init_row = np.e**(j*k0*x_vals) * np.e**(-((x_vals-x0)**2)/(2*(sigma**2))) / (np.sqrt(sigma*np.sqrt(np.pi)))
     init_row /= np.sqrt(prob_integral(init_row)) # Making sure initial condition is normalized
-    probabilities[0] = (prob_integral(init_row)) # initial probability 
+
+    # Setting initial conditions and probability
+    probabilities[0] = (prob_integral(init_row)) 
     psi[:,0] = init_row  
+
+    # Creating matrices for calculation
     I = make_tridiagonal(nspace,0,1,0) # identity matrix
     H = make_tridiagonal(nspace,-1,2,-1)
     np.fill_diagonal(H, np.diagonal(H) + V) # equation 9.31 in textbook with h_bar and m as 1 and 1/2
@@ -94,7 +102,7 @@ def sch_eqn(nspace, ntime, tau, method = 'ftcs', length = 200, potential = [], w
     crank_matrix_1 = np.linalg.inv(I + (j*tau/2 * H))
     crank_matrix_2 = I - (j*tau/2 * H) 
 
-    # Checking if solution would be stable for ftcs
+    # Checking if solution would be stable for ftcs if selected method 
     calculate = True 
     if method.lower() == 'ftcs' and spectral_radius(ftcs_matrix) > 1:
 
@@ -122,18 +130,22 @@ def sch_eqn(nspace, ntime, tau, method = 'ftcs', length = 200, potential = [], w
 
 #print(sch_eqn(100,500,1e-5,'ftcs'))
 
-# Plotting Function
+
+
+# Plotting Function 
 
 def sch_plot(nspace, ntime, tau, plottype, specific_time, method = 'ftcs', length = 200, potential = [], wparam = [10,0,0.5],save='no'):
     '''Function that plots results for schrodinger equation. Please provide the following in order:
-    number of spatial grid points, number of time steps to be evolved, the time step to be used, the plot you want (string) either
-    psi( a plot of the real part of ψ(x) at a specific time t)  or prob (prob, a plot of the particle probability density ψ ψ*(x) at a specific time), and the specific
-    time for when you want the plot.
-     Optional arguments are:
-    1. method: string, either ftcs or crank (Crank-Nicolson), 2. length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100),
-    3. potential: 1-D array giving the spatial index values at which the potential V(x) should be set to 1. Default to empty. ,
-    4. wparam: list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
-    4. save: string either yes or no if you want the plot to be saved. Default is no.
+    1. nspace: int, number of spatial grid points, 
+    2. ntime: int, number of time steps to be evolved, 
+    3. tau: float, the time step to be used, 
+    4. plottype: string, the plot you want, either psi( a plot of the real part of ψ(x) at a specific time t)  or prob (prob, a plot of the particle probability density ψ ψ*(x) at a specific time), 
+    5. specific_time: float, the specific time for when you want the plot. Will be rounded to nearest multiple of tau if not initially an integer multiple of tau.
+        The rest are optional arguments and are:
+    6. method: string, either ftcs or crank (Crank-Nicolson), 2. length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100),
+    7. potential: array, 1-D array giving the spatial index values at which the potential V(x) should be set to 1. Default to empty. ,
+    8. wparam: list, list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
+    9. save: string, either yes or no if you want the plot to be saved. Default is no.
     
     Unit assumptions h_bar = 1 and m = 1/2. 
 
@@ -144,12 +156,12 @@ def sch_plot(nspace, ntime, tau, plottype, specific_time, method = 'ftcs', lengt
     psi_matrix = results[0]
     x_vals = results[1]
     t_vals = results[2]
-    probabilities = results[3]
+    probabilities = results[3] 
     time_range = ntime * tau
 
     # Finding index of specific time in n time
     timeindex = None
-    tolerance = 1e-5
+    tolerance = tau/2 # setting tolerance to find nearest multiple of tau
 
     for timepoint in range(ntime):
         if abs(specific_time - timepoint * tau) < tolerance:
@@ -157,31 +169,49 @@ def sch_plot(nspace, ntime, tau, plottype, specific_time, method = 'ftcs', lengt
             break
     
     if timeindex is None:
-        print(f'Error: specific_time ({specific_time}) must be a multiple of time step tau ({tau}).')
+
         print(f'Valid time steps are multiples of {tau}, and should be within the time range {time_range}.')
         return
     
     if plottype.lower() == 'psi':
 
         real_at_t = np.real(psi_matrix[:,timeindex])
+        # Plotting Real psi
         plt.plot(x_vals, real_at_t)
+        plt.xlabel('x')
+        plt.ylabel(f'Real part of ψ(x)')
+        plt.title(f'Plot of the real part of ψ(x) at a time t = {specific_time}')
+
         plt.show()
 
     if plottype.lower() == 'prob':
 
         prob_dens = psi_matrix[:,timeindex] * np.conj(psi_matrix[:,timeindex])
+        area = prob_integral(psi_matrix[:,timeindex]) # Recomputing probability just to show that it is conserved
+        # Plotting Probability Density
         plt.plot(x_vals, prob_dens)
-        
-        area2 = prob_integral(psi_matrix[:,timeindex])
-        print(f"Area under the curve: {round(area2,3)}")
+        plt.xlabel('x')
+        plt.ylabel(f'Probability Density  ψ ψ*(x)')
+        plt.title(f'Probability Density, ψ ψ*(x) at t = {specific_time}, Area Under Curve = {np.round(area,3)}')
         plt.show()
 
-    X, T = np.meshgrid(x_vals, t_vals)
 
-    return results
+    ### Extra Code I Wrote To See How Probability Evolves ### Inspired By Lab 9
+
+    # X, T = np.meshgrid( t_vals , x_vals)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.plot_surface(X, T, psi_matrix[:,:] * np.conj(psi_matrix[:,:]), cmap='viridis', edgecolor='none')
+    # ax.set_ylabel('x position')
+    # ax.set_xlabel('Time (s)')
+    # ax.set_zlabel('Probability Density')
+    # ax.set_title('3D Evolution of Probability Density')
+    # plt.show()
+
+    return 
+
+
 ex = range(0,1000)
 
-#sch_plot(100,900,1e-1,'psi',0,'crank',length=50)
-sch_plot(500,9000,1e-1,'prob',800,'crank',length=50)
-
-#increasing number of points in nspace decreases my probability somehow
+sch_plot(200,9000,1e-1,'prob',80.05,'crank',length=250)
+#sch_plot(500,9000,1e-1,'prob',800.05,'crank',length=200)
